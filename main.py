30795f40019ecd83970fd8935e0f30c711a0b972
Sanyavas <vasilinaoleksandr@gmail.com>
2024-12-19 17:33:53 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,14 @@
+import uvicorn
+
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+def root():
+    return {"message": "Welcome to FastAPI!"}
+
+
+if __name__ == '__main__':
+    uvicorn.run("main:app", host="127.0.0.1", port=8001, reload=True)
