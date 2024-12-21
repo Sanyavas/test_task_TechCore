a0ef7a5563e34652522d5d969723a20c71e7deeb
Sanyavas <vasilinaoleksandr@gmail.com>
2024-12-21 14:15:39 +0200

    fix models

--- src/db/models.py
+++ src/db/models.py
@@ -9,10 +9,10 @@ Base = declarative_base()
 class Product(Base):
     __tablename__ = 'products'
 
-    product_id: Mapped[int] = mapped_column(primary_key=True)
+    product_id: Mapped[int] = mapped_column(primary_key=True, index=True)
     name: Mapped[str] = mapped_column(String())
     description: Mapped[str] = mapped_column(String())
     price: Mapped[Decimal] = mapped_column(Numeric(10, 2), nullable=False, unique=False)
-    external_id: Mapped[int] = mapped_column(Integer, nullable=False)
+    external_id: Mapped[int] = mapped_column(Integer, nullable=False, index=True)
     created_at: Mapped[datetime] = mapped_column('created_at', DateTime, default=func.now())
     updated_at: Mapped[datetime] = mapped_column('updated_at', DateTime, default=func.now(), onupdate=func.now())
--- src/schemas/products.py
+++ src/schemas/products.py
@@ -4,7 +4,6 @@ from decimal import Decimal
 from pydantic import BaseModel, Field
 
 class ProductModel(BaseModel):
-
     name: str
     description: str
     price: Decimal
@@ -12,7 +11,6 @@ class ProductModel(BaseModel):
 
 
 class ProductResponse(ProductModel):
-
     product_id: int
     created_at: datetime = Field(default_factory=datetime.now)
     updated_at: datetime = Field(default_factory=datetime.now)
